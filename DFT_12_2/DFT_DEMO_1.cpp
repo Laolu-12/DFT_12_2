5d3e0a14b0a8ce2fd68ca18c500806b8796b71c9
Laolu-12 <1187891142@qq.com>
2020-12-02 21:17:13 +0800

    111

---
+++ DFT_12_2/DFT_DEMO_1.cpp
@@ -0,0 +1,20 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+#include "functions.h"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	//开始计时
+
+	//dftDemo();
+	//mouseROI();
+	ifftDemo();
+	//结束计时
+	//显示时间
+
+	//等待键盘响应，按任意键结束程序
+	return 0;
+}
